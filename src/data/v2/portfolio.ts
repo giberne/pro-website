6420c11651e4b2532909aa4e009a8d20a0d885a8
giberne <gibernethomas@gmail.com>
2026-02-03 19:41:19 +0100

    added portfolio to the portfolio lol

--- src/data/v2/portfolio.ts
+++ src/data/v2/portfolio.ts
@@ -11,7 +11,57 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    id: 1,
+    title: 'Portfolio interactif & blog',
+    subtitle: 'Une interface web expérimentale mêlant design, animation.',
+    tags: 'Site Web - UI/UX - Plateforme Gaming',
+    features: [
+      'Page d’accueil animée avec effets de survol.',
+      'Interface inspirée du style BentoBox.',
+      'Blog dynamique connecté à un CMS.',
+      'Interface claire, rapide et responsive.',
+      'Optimisation des performances et des animations.',
+    ],
+    result:
+    'Une page d’accueil pensée comme un véritable point d’entrée produit.',
+    link: 'https://akisama.fr',
+    image: '/portfolio/akisama-fr.png',
+  },
+  {
     id: 2,
+    title: 'Portfolio créatif animé',
+    subtitle: 'Portfolio moderne avec animations Framer Motion',
+    tags: 'Site Web - UI/UX - Framer Motion - CMS',
+    features: [
+      'Développement fidèle d\'une maquette fournie.',
+      'Animations fluides réalisées avec Framer Motion.',
+      'CMS dynamique pour gérer projets et contenus.',
+      'Mise en page pixel-perfect.',
+      'Site responsive et fluide sur tous les appareils.',
+    ],
+    result: 'Un portfolio moderne, animé et entièrement administrable. Développement rapide et de qualité.',
+    link: 'https://agisquet.com/',
+    image: '/portfolio/image 49.png',
+  },
+  {
+    id: 3,
+    title: 'Gaming Playlyst',
+    subtitle: 'Moteur de recherche de jeux vidéo',
+    tags: 'Site Web - UI/UX - Plateforme Gaming',
+    features: [
+      'Recherche parmi plus de 300 000 jeux.',
+      'Fiches jeux accessibles en français.',
+      'Mise en avant des jeux du moment.',
+      'Interface claire, rapide et responsive.',
+      'Accès à la bêta mobile via une page dédiée.',
+    ],
+    result:
+    'Une page d’accueil pensée comme un véritable point d’entrée produit.',
+    link: 'https://playlyst.akisama.fr',
+    image: '/portfolio/playlyst-home.png',
+  },
+  {
+    id: 4,
     title: '1HEURE42',
     subtitle: 'Site vitrine immersif pour DJ',
     tags: 'Développement Web - UI/UX - CMS sur mesure',
@@ -25,40 +75,5 @@ export const projects: Project[] = [
     result: 'Un site professionnel, immersif et simple à gérer. Design unique et moderne.',
     link: 'https://1heure42.vercel.app',
     image: '/portfolio/Rectangle 545.png',
-  },
-  {
-    id: 1,
-    title: 'Portfolio créatif animé',
-    subtitle: 'Portfolio moderne avec animations Framer Motion',
-    tags: 'Site Web - UI/UX - Framer Motion - CMS',
-    features: [
-      'Développement fidèle d\'une maquette fournie.',
-      'Animations fluides réalisées avec Framer Motion.',
-      'CMS dynamique pour gérer projets et contenus.',
-      'Mise en page pixel-perfect.',
-      'Déploiement rapide et performant.',
-      'Site responsive et fluide sur tous les appareils.',
-    ],
-    result: 'Un portfolio moderne, animé et entièrement administrable. Développement rapide et de qualité.',
-    link: 'https://agisquet.com/',
-    image: '/portfolio/image 49.png',
-  },
-  {
-    id: 4,
-    title: 'Gaming Playlyst',
-    subtitle: 'Moteur de recherche de jeux vidéo',
-    tags: 'Site Web - UI/UX - Plateforme Gaming',
-    features: [
-      'Recherche instantanée parmi plus de 300 000 jeux.',
-      'Fiches jeux accessibles en français.',
-      'Mise en avant des jeux du moment.',
-      'Interface claire, rapide et responsive.',
-      'Présentation des fonctionnalités de l’application mobile.',
-      'Accès à la bêta mobile via une page dédiée.',
-    ],
-    result:
-      'Une page d’accueil pensée comme un véritable point d’entrée produit.',
-    link: 'https://playlyst.akisama.fr',
-    image: '/portfolio/playlyst-home.png',
   },
 ]
